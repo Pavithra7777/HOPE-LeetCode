708f89a7a1ddfc21b9c9104a205c6147723c0285
Pavithra <pavithraelumalai567@gmail.com>
2026-04-13 10:06:08 +0530

    Create Loose(Aggreation).java

---
+++ Day 12/Loose(Aggreation).java
@@ -0,0 +1,24 @@
+class Driver{
+    String name;
+    Driver(String name){//2
+        this.name=name;//3
+    }
+    void drive(){//10
+        System.out.println("Driver "+name+" came");//11
+    }
+}
+class Car{
+    Driver d;                                         
+    Car(Driver d){//5
+        this.d=d;//6
+    }
+    void start(){//8                              
+        d.drive();//9                              
+        System.out.println("car has started");//12
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Driver d=new Driver("Rock");//1
+        Car c=new //7
+    }
